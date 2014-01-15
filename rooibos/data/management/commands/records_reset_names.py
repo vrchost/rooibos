cfad4a695ef44e339b83cc2f1d2c86e2b64c8ff8
Andreas Knab <knabar@gmail.com>
2014-01-15 18:51:33 +0100

    Add name reset commands for records and media

---
+++ rooibos/data/management/commands/records_reset_names.py
@@ -0,0 +1,31 @@
+from optparse import make_option
+from django.core.management.base import BaseCommand
+from rooibos.data.models import Record, FieldValue, standardfield
+from rooibos.util.progressbar import ProgressBar
+
+from django.template.defaultfilters import slugify
+
+class Command(BaseCommand):
+    help = 'Resets the name property of all Record objects'
+
+
+    def handle(self, *args, **kwargs):
+
+        updated = 0
+
+        id_fields = standardfield('identifier', equiv=True)
+        titles = FieldValue.objects.select_related('record').filter(field__in=id_fields)
+        pb = ProgressBar(titles.count())
+
+        for count, title in enumerate(titles):
+            name = slugify(title.value)
+            if name != title.record.name:
+                title.record.name = name
+                title.record.save(force_update_name=True)
+                updated += 1
+
+            pb.update(count)
+
+        pb.done()
+
+        print "Updated %d record objects" % updated
---
+++ rooibos/storage/management/commands/media_reset_names.py
@@ -0,0 +1,28 @@
+from optparse import make_option
+from django.core.management.base import BaseCommand
+from rooibos.storage.models import Media
+from rooibos.util.progressbar import ProgressBar
+
+import os
+from django.template.defaultfilters import slugify
+
+class Command(BaseCommand):
+    help = 'Resets the name property of all Media objects'
+
+
+    def handle(self, *args, **kwargs):
+
+        updated = 0
+        pb = ProgressBar(Media.objects.count())
+        for count, media in enumerate(Media.objects.all()):
+            name = slugify(os.path.splitext(os.path.basename(media.url))[0])
+            if name != media.name:
+                media.name = name
+                media.save(force_update_name=True)
+                updated += 1
+
+            pb.update(count)
+
+        pb.done()
+
+        print "Updated %d media objects" % updated
