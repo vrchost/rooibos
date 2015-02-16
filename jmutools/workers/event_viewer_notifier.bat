cc5d5e9f6d91e2c375215d750fad6916671b1b05
Leighton Shank <shanklt@jmu.edu>
2015-02-16 11:31:02 -0500

    Fix XPath query for HipChat event notifications
    So the batch file that processes the events to send to the HipChat
    notifier uses the wevtutil.exe util to pull event details.  The only
    issue was that the XPath query was hard-coded to look in the 'System'
    path, which works for the Service Control events, but the actual 'MDID
    Workers' events come through the Application path.
    
    The event_viewer_notifier.bat file receives the Channel as an argument,
    so I'm using that to specify the Path attribute to the <Query> and
    <Select> tags.

--- jmutools/workers/event_viewer_notifier.bat
+++ jmutools/workers/event_viewer_notifier.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 
-wevtutil qe %1 /f:RenderedXML /q:"<QueryList><Query Id='0' Path='System'><Select Path='System'>*[System[(EventRecordID=%2)]]</Select></Query></QueryList>" | python hipchat_event_notifier.py
+wevtutil qe %1 /f:RenderedXML /q:"<QueryList><Query Id='0' Path='%1'><Select Path='%1'>*[System[(EventRecordID=%2)]]</Select></Query></QueryList>" | python hipchat_event_notifier.py
