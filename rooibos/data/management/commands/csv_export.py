753e1a91024234b626f6054c078fc2dac42003a1
Andreas Knab <knabar@gmail.com>
2015-08-17 10:36:58 +0200

    Add columns with media url and storage path to CSV export

--- rooibos/data/management/commands/csv_export.py
+++ rooibos/data/management/commands/csv_export.py
@@ -30,6 +30,7 @@ class Command(BaseCommand):
         with open(data_file, 'w') as csvfile:
 
             fieldnames = [field.full_name for field in fields]
+            fieldnames.extend(['__file__', '__path__'])
 
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
@@ -40,21 +41,30 @@ class Command(BaseCommand):
             for count, record in enumerate(records):
 
                 values = record.get_fieldvalues()
-                while values:
+                media = list(record.media_set.select_related('storage').all())
+
+                while values or media:
                     row = dict()
-                    extra = list()
+                    extra_values = list()
+
                     for value in values:
                         fieldname = value.field.full_name
                         v = value.value.encode('utf8')
                         if fieldname in row:
                             if not separator:
-                                extra.append(value)
+                                extra_values.append(value)
                             else:
                                 row[fieldname] += separator + v
                         else:
                             row[fieldname] = v
+
+                    if media:
+                        m = media.pop()
+                        row['__file__'] = m.url
+                        row['__path__'] = m.storage.base
+
                     writer.writerow(row)
-                    values = extra
+                    values = extra_values
 
                 pb.update(count)
 
