af165426f61502279bb4e59bc395191a219c328f
Andreas Knab <knabar@gmail.com>
2016-03-27 12:56:17 +0200

    flake8 cleanup of groupmanager app

--- rooibos/groupmanager/tests.py
+++ rooibos/groupmanager/tests.py
@@ -2,12 +2,16 @@ from django.test import TestCase
 from django.contrib.auth.models import User, Group
 from management.commands.managegroup import Command
 
+
 class TestManageGroupCommand(TestCase):
 
     def setUp(self):
-        self.user1 = User.objects.create(username='TestManageGroupCommand-user1')
-        self.user2 = User.objects.create(username='TestManageGroupCommand-user2')
-        self.user3 = User.objects.create(username='TestManageGroupCommand-user3')
+        self.user1 = User.objects.create(
+            username='TestManageGroupCommand-user1')
+        self.user2 = User.objects.create(
+            username='TestManageGroupCommand-user2')
+        self.user3 = User.objects.create(
+            username='TestManageGroupCommand-user3')
         self.newusername = 'TestManageGroupCommand-newuser'
 
     def tearDown(self):
@@ -34,10 +38,13 @@ class TestManageGroupCommand(TestCase):
         group = Group.objects.create(name='test_simple_group_members')
         group.user_set.add(self.user1)
         group.user_set.add(self.user3)
-        Command().handle(usergroup='test_simple_group_members',
-                         users=','.join([self.user1.username, self.user2.username,
-                                   'this-user-does-not-exist']),
-                         output=output)
+        Command().handle(
+            usergroup='test_simple_group_members',
+            users=','.join(
+                [self.user1.username, self.user2.username,
+                 'this-user-does-not-exist']),
+            output=output
+        )
         self.assertEqual(2, len(group.user_set.all()))
         self.assertEqual(1, len(output))
 
@@ -50,9 +57,10 @@ class TestManageGroupCommand(TestCase):
                          output=output)
         self.assertEqual(2, len(group.user_set.all()))
         self.assertEqual(0, len(output))
-        Command().handle(usergroup='test_add_group_members',
-                         addusers=[','.join([self.user2.username, self.user3.username])],
-                         output=output)
+        Command().handle(
+            usergroup='test_add_group_members',
+            addusers=[','.join([self.user2.username, self.user3.username])],
+            output=output)
         self.assertEqual(3, len(group.user_set.all()))
         self.assertEqual(0, len(output))
 
@@ -61,20 +69,24 @@ class TestManageGroupCommand(TestCase):
         group = Group.objects.create(name='test_remove_group_members')
         group.user_set.add(self.user1)
         group.user_set.add(self.user2)
-        Command().handle(usergroup='test_remove_group_members',
-                         removeusers=[self.user1.username, self.user3.username],
-                         output=output)
+        Command().handle(
+            usergroup='test_remove_group_members',
+            removeusers=[self.user1.username, self.user3.username],
+            output=output)
         self.assertEqual(1, len(group.user_set.all()))
         self.assertEqual(0, len(output))
 
     def test_create_users(self):
         output = []
         group = Group.objects.create(name='test_create_users')
-        self.assertEqual(0, User.objects.filter(username=self.newusername).count())
-        Command().handle(usergroup='test_create_users',
-                         addusers=['%s:test@example.com:John:Doe:changeme' % self.newusername],
-                         createusers=True,
-                         output=output)
+        self.assertEqual(0, User.objects.filter(
+            username=self.newusername).count())
+        Command().handle(
+            usergroup='test_create_users',
+            addusers=['%s:test@example.com:John:Doe:changeme' %
+                      self.newusername],
+            createusers=True,
+            output=output)
         self.assertEqual(1, len(group.user_set.all()))
         users = User.objects.filter(username=self.newusername)
         self.assertEqual(1, users.count())
