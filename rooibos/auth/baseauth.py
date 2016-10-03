5c62b83f6aff627173ee3a94ae9300d5077cac8e
Andreas Knab <knabar@gmail.com>
2016-10-03 09:23:02 +0200

    Fix user account creation

--- rooibos/auth/baseauth.py
+++ rooibos/auth/baseauth.py
@@ -11,13 +11,13 @@ class BaseAuthenticationBackend:
 
     def _create_user(self, username, password=None,
                      first_name=None, last_name=None, email=None):
-        password = password or ''.join(
-            Random().sample(string.letters + string.digits, 20))
         last_name = last_name or username
         user = User(username=username, password=password)
         user.first_name = first_name and first_name[:30] or ''
         user.last_name = last_name[:30]
         user.email = email
+        if not password:
+            user.set_unusable_password()
         user.save()
         return user
 
