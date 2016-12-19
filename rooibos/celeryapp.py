476b0d0980d1bd2ada8674ec78f06036543cae89
Andreas Knab <knabar@gmail.com>
2016-12-19 20:12:05 +0100

    Move owned_task decorator

--- rooibos/celeryapp.py
+++ rooibos/celeryapp.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 from celery import Celery
+from django.contrib.auth.models import User
+from rooibos.workers.models import TaskOwnership
 
 
 app = Celery('rooibos')
@@ -17,3 +19,39 @@ app.autodiscover_tasks()
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
+
+def owned_task(*args, **kwargs):
+
+    def create_owned_task(userarg='owner', removeuserarg=False, **kwargs):
+
+        def wrapper(func):
+            kwargs['bind'] = True
+
+            @app.task(**kwargs)
+            def wrapped(self, *args, **kwargs):
+                if not self.request.called_directly and userarg in kwargs:
+                    user = kwargs[userarg]
+                    if not isinstance(user, User):
+                        user = User.objects.get(username=user)
+                    TaskOwnership.objects.get_or_create(
+                        task_id=self.request.id,
+                        owner=user,
+                    )
+                    if removeuserarg:
+                        del kwargs[userarg]
+                return func(self, *args, **kwargs)
+
+            return wrapped
+
+        return wrapper
+
+    if len(args) == 1:
+        if callable(args[0]):
+            return create_owned_task(**kwargs)(*args)
+        raise TypeError('argument 1 to @owned_task() must be a callable')
+    if args:
+        raise TypeError(
+            '@owned_task() takes exactly 1 argument ({0} given)'.format(
+                sum([len(args), len(kwargs)])))
+    return create_owned_task(**kwargs)
