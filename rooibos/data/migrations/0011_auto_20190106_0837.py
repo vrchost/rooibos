32b2c7a2e7871bd2e34df017bd4b306b62d27417
Andreas Knab <knabar@gmail.com>
2019-01-06 15:38:17 +0100

    Add missing migration

---
+++ rooibos/data/migrations/0011_auto_20190106_0837.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('data', '0010_remove_dc_vra_equivalent_field'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='collection',
+            name='password',
+            field=models.CharField(max_length=32, serialize=False, blank=True),
+        ),
+    ]
