d79bcc036c4e6ec73b9d50a2fa5e18f71da26fbd
Andreas Knab <knabar@gmail.com>
2019-02-10 13:25:49 +0100

    Reindex records when their permissions change

--- rooibos/solr/models.py
+++ rooibos/solr/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models.signals import post_delete, post_save
 from django.contrib.contenttypes.models import ContentType
+from rooibos.access.models import AccessControl
 from rooibos.data.models import Record, CollectionItem
 from tagging.models import TaggedItem
 from rooibos.util.models import OwnedWrapper
@@ -40,6 +41,12 @@ def post_collectionitem_callback(sender, **kwargs):
     mark_for_update(record_id=kwargs['instance'].record_id)
 
 
+def post_accesscontrol_callback(sender, instance, **kwargs):
+    record_type = ContentType.objects.get_for_model(Record)
+    if instance and instance.content_type == record_type:
+        mark_for_update(record_id=instance.object_id)
+
+
 post_delete.connect(post_record_delete_callback, sender=Record)
 post_save.connect(post_record_save_callback, sender=Record)
 
@@ -48,6 +55,9 @@ post_save.connect(post_taggeditem_callback, sender=TaggedItem)
 
 post_delete.connect(post_collectionitem_callback, sender=CollectionItem)
 post_save.connect(post_collectionitem_callback, sender=CollectionItem)
+
+post_delete.connect(post_accesscontrol_callback, sender=AccessControl)
+post_save.connect(post_accesscontrol_callback, sender=AccessControl)
 
 
 def disconnect_signals():
@@ -59,3 +69,6 @@ def disconnect_signals():
 
     post_delete.disconnect(post_collectionitem_callback, sender=CollectionItem)
     post_save.disconnect(post_collectionitem_callback, sender=CollectionItem)
+
+    post_delete.disconnect(post_accesscontrol_callback, sender=AccessControl)
+    post_save.disconnect(post_accesscontrol_callback, sender=AccessControl)
