816f12aaee0b986ee981a62f4fbae55d67791a55
Andreas Knab <knabar@gmail.com>
2019-06-26 18:14:48 +0200

    Add extra index on intermediary table for better performance

---
+++ rooibos/data/migrations/0012_add_equivalent_index.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('data', '0011_auto_20190106_0837'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            "CREATE INDEX idx_from_field_id ON data_field_equivalent (from_field_id)",
+            "DROP INDEX idx_from_field_id ON data_field_equivalent"
+        ),
+    ]
