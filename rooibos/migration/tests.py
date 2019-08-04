5ac12415125818b0d0e059f22408fa0f2fa4b828
Andreas Knab <knabar@gmail.com>
2019-08-04 21:29:47 +0200

    Disable test that relies on pydobc

--- rooibos/migration/tests.py
+++ rooibos/migration/tests.py
@@ -1,24 +1,24 @@
-import unittest
-from management.commands.mdid2migrate import MigrateUsers
-
-
-class UserTestCase(unittest.TestCase):
-
-    def test_no_email(self):
-
-        class DummyRow(object):
-            def __init__(self):
-                self.Login = 'test_UNIQUE98659856298356'
-                self.Password = None
-                self.Name = 'Test'
-                self.FirstName = 'Test'
-                self.Email = None
-                self.Administrator = False
-                self.LastAuthenticated = None
-
-        row = DummyRow()
-        migrate = MigrateUsers(None)
-        user = migrate.create_instance(row)
-        migrate.update(user, row)
-
-        user.save()
+# import unittest
+# from management.commands.mdid2migrate import MigrateUsers
+#
+#
+# class UserTestCase(unittest.TestCase):
+#
+#     def test_no_email(self):
+#
+#         class DummyRow(object):
+#             def __init__(self):
+#                 self.Login = 'test_UNIQUE98659856298356'
+#                 self.Password = None
+#                 self.Name = 'Test'
+#                 self.FirstName = 'Test'
+#                 self.Email = None
+#                 self.Administrator = False
+#                 self.LastAuthenticated = None
+#
+#         row = DummyRow()
+#         migrate = MigrateUsers(None)
+#         user = migrate.create_instance(row)
+#         migrate.update(user, row)
+#
+#         user.save()
