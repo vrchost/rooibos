21459ec3fc6b026daa0892c181cf7613eee20f49
Andreas Knab <knabar@gmail.com>
2020-05-02 20:53:18 +0200

    Remove print statement

--- rooibos/presentation/viewers.py
+++ rooibos/presentation/viewers.py
@@ -468,7 +468,6 @@ class PackageFilesViewer(Viewer):
                         ext,
                     )
                 )
-                print(outname)
                 output.write(
                     image, outname.encode('ascii', 'replace').decode()
                 )
