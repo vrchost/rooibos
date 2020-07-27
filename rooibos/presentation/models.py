9a8ae00bbd11f8cae2721b28f89bed198e06f642
Andreas Knab <knabar@gmail.com>
2020-07-27 14:22:39 +0200

    Presentation IDs in passwords dict are strings

--- rooibos/presentation/models.py
+++ rooibos/presentation/models.py
@@ -101,7 +101,7 @@ class Presentation(models.Model):
         self.unlocked = (
             (self.owner == request.user) or
             (not self.password) or
-            (request.session.get('passwords', dict()).get(self.id) ==
+            (request.session.get('passwords', dict()).get(str(self.id)) ==
              self.password)
         )
         return self.unlocked
