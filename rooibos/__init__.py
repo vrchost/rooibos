a014a0ac5a2091b45ee92ad04f6191ebfb424875
Andreas Knab <knabar@gmail.com>
2021-01-31 15:40:45 +0100

    Don't rely on any modules during package build

--- rooibos/__init__.py
+++ rooibos/__init__.py
@@ -1,11 +1,16 @@
-from django.core.exceptions import ImproperlyConfigured
-
 try:
-    # This will make sure the app is always imported when
-    # Django starts so that shared_task will use this app.
-    from .celeryapp import app as celery_app
-except ImproperlyConfigured:
-    # Don't break when running outside of configured Django environment
+    from django.core.exceptions import ImproperlyConfigured
+
+    try:
+        # This will make sure the app is always imported when
+        # Django starts so that shared_task will use this app.
+        from .celeryapp import app as celery_app
+    except ImproperlyConfigured:
+        # Don't break when running outside of configured Django environment
+        pass
+
+except ModuleNotFoundError:
+    # Running in setup mode
     pass
 
 
