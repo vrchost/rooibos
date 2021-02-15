fc1746ebaed6f06f7f66cbfc4202de6dff3a6195
Andreas Knab <knabar@gmail.com>
2021-02-15 11:40:49 +0100

    Add field usage command

---
+++ rooibos/data/management/commands/field_usage.py
@@ -0,0 +1,25 @@
+import csv
+import io
+
+from django.core.management.base import BaseCommand
+
+from rooibos.data.models import FieldValue, Field, Collection
+
+
+class Command(BaseCommand):
+    help = 'Outputs usage of fields in collections for further processing in ' \
+           'a pivot table'
+
+    def handle(self, *args, **kwargs):
+
+        outfile = io.StringIO()
+        outcsv = csv.writer(outfile)
+        outcsv.writerow(['Collection', 'Field', 'Count'])
+
+        for f in Field.objects.all():
+            for c in Collection.objects.all():
+                count = FieldValue.objects.filter(field=f, record__collection=c).count()
+                if count > 0:
+                    outcsv.writerow([c.title, f.full_name, str(count)])
+
+        print(outfile.getvalue())
