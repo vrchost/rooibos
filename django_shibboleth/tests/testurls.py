317d46d6a28fdbd86e22761734c57ff9a361aaca
Andreas Knab <knabar@gmail.com>
2021-03-08 21:38:36 +0100

    Remove embedded django_shibboleth test suite

--- django_shibboleth/tests/testurls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls.defaults import *
-
-urlpatterns = patterns('',
-    url(r'^login$', 'django_shibboleth.views.shib_register',),
-    url(r'^success$', 'django_shibboleth.tests.testviews.success',),
-)
