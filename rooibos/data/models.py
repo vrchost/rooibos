79db6864566f0853bfdaff68553e5246eb712c51
Andreas Knab <knabar@gmail.com>
2021-11-16 15:22:44 +0100

    Fix deprecation warning

--- rooibos/data/models.py
+++ rooibos/data/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.db import models, transaction
 from django.db.models import Q, signals
 from django.core.exceptions import MultipleObjectsReturned
