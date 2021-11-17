648f941717dc982396f9f3c3bf75e632f20d6034
Andreas Knab <knabar@gmail.com>
2021-11-17 21:10:47 +0100

    Update requirements

--- setup.py
+++ setup.py
@@ -1,56 +1,17 @@
+import os.path
+import re
 import setuptools
 from rooibos.version import get_version
 
 
-REQUIRES = """
-attrs>=17.4.0
-b2sdk==0.1.6
-beautifulsoup4
-bleach==3.1.4
-bleach-allowlist==1.0.3
-boto
-Celery==4.2.1
-configobj>=4.7.2,<=5.0.0
-cryptography
-django-appconf==1.0.3
-django-cas-ng==3.5.10
-django-compressor==2.4.1
-django-extensions==2.0.7
-django-pagination==1.0.7
-django-ranged-fileresponse==0.1.2
-django-storages==1.6.6
-django-tagging==0.4.6
-django-contrib-comments==1.8.0
-Django==2.0.13
-django-celery-results==1.0.4
-django-cors-headers==2.5.3
-flake8
-flake8-docstrings
-flake8-todo
-flickrapi>=2
-flup
-gunicorn
-ipaddr
-markdown==3.1.1
-pymysql
-pep8
-pep8-naming
-pika
-pillow
-pycryptodome
-PyPDF2
-python-ldap
-python-memcached
-python-pptx
-reportlab
-requests
-sqlparse
-werkzeug==0.16.0
-wfastcgi
-wheel
-requests
-unicodecsv==0.14.1
-"""
+requirements = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+
+with open(requirements) as r:
+    REQUIRES = [
+        package
+        for package in r.readlines()
+        if re.match(r'^[a-z]', package, re.IGNORECASE)
+    ]
 
 
 setuptools.setup(
